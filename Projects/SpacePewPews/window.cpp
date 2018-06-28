7662709c51a79437c808594359ff2266a4cfef6b
Shady Dealer <dimiturlukanov@gmail.com>
2018-06-28 14:07:05 +0300

    Made the map generating method recognize entities.

--- Projects/SpacePewPews/window.cpp
+++ Projects/SpacePewPews/window.cpp
@@ -3,43 +3,111 @@
 
 #include "window.h"
 
+#pragma region Big Four
+
 Window::Window() : width(DEFAULT_WIDTH + 3),   //+3 for the two borders and the null terminator
                    height(DEFAULT_HEIGHT + 2), //+2 for the two border rows.
-                   map(width, height)
+                   map(width, height),
+                   bullets(),
+                   enemies(),
+                   player(0,
+                          0,
+                          RIGHT,
+                          DEFAULT_PLAYER_AVATAR_PATH)
 {
-    generate_map();
-}
-
-Window::Window(const int rhsWidth,
-               const int rhsHeight) : width(rhsWidth + 1),
-                                      height(rhsHeight + 1),
-                                      map(width, height)
-{
+    set_entity_starting_position(&player, height);
     generate_map();
 }
 
 Window::~Window()
 {
 }
+#pragma endregion
+
+Window::Window(const int rhsWidth,
+               const int rhsHeight) : width(rhsWidth + 3),
+                                      height(rhsHeight + 2),
+                                      map(width, height),
+                                      bullets(),
+                                      enemies(),
+                                      player(0,
+                                             0,
+                                             RIGHT,
+                                             DEFAULT_PLAYER_AVATAR_PATH)
+{
+    if (height - 2 >= player.get_avatar().get_data().get_height() &&
+        width - 3 >= player.get_avatar().get_data().get_width())
+    {
+        set_entity_starting_position(&player, height);
+        generate_map();
+    }
+    else
+    {
+        throw InvalidMapSize(player.get_avatar().get_data().get_height(),
+                             player.get_avatar().get_data().get_width());
+    }
+}
+
+void Window::set_entity_starting_position(Entity *rhs, const int rhsHeight)
+{
+    rhs->move(rhs->get_avatar().get_data().get_width() / 2 + (rhs->get_avatar().get_data().get_width() % 2 != 0),
+              rhsHeight / 2);
+}
 
 /*
-    Generates the base map.
+    Generates the map for each frame.
  */
 void Window::generate_map()
 {
-    for (int c = 0; c < width - 1; ++c)
-        map[0][c] = VERTICAL_BORDER_TILE;
+    LinkedList<Bullet>::iterator bulletIt = bullets.beg();
+    LinkedList<Character>::iterator enemyIt = enemies.beg();
 
-    for (int r = 1; r < height - 1; ++r)
+    Pair<int, int> indexes(0, 0);
+    int row = 0, col = 0;
+
+    for (indexes.first = 0; indexes.first < width - 1; ++indexes.first)
+        map[0][indexes.first] = HORIZONTAL_BORDER_TILE;
+
+    for (indexes.second = 1; indexes.second < height - 1; ++indexes.second)
     {
-        for (int c = 1; c < width - 2; ++c)
-            map[r][c] = EMPTY_TILE;
+        for (indexes.first = 1; indexes.first < width - 2; ++indexes.first)
+        {
+            if (bulletIt != bullets.end())
+            {
+                if ((*bulletIt).within_hitbox(indexes))
+                {
+                    row = indexes.second - (((*bulletIt).get_coords().second - ((*bulletIt).get_avatar().get_data().get_height() / 2)));
+                    col = indexes.first - (((*bulletIt).get_coords().first - ((*bulletIt).get_avatar().get_data().get_width() / 2)));
+                    map[indexes.second][indexes.first] = (*bulletIt).get_avatar().get_data()[row][col];
 
-        map[r][0] = map[r][width - 2] = HORIZONTAL_BORDER_TILE;
+                    ++bulletIt;
+                }
+            }
+            else if (enemyIt != enemies.end())
+            {
+                if ((*enemyIt).within_hitbox(indexes))
+                {
+                    row = indexes.second - (((*enemyIt).get_coords().second - ((*enemyIt).get_avatar().get_data().get_height() / 2)));
+                    col = indexes.first - (((*enemyIt).get_coords().first - ((*enemyIt).get_avatar().get_data().get_width() / 2)));
+                    map[indexes.second][indexes.first] = (*enemyIt).get_avatar().get_data()[row][col];
+
+                    ++enemyIt;
+                }
+            }
+            else if (player.within_hitbox(indexes))
+            {
+                row = indexes.second - (player.get_coords().second - player.get_avatar().get_data().get_height() / 2);
+                col = indexes.first - (player.get_coords().first - player.get_avatar().get_data().get_width() / 2);
+                map[indexes.second][indexes.first] = player.get_avatar().get_data()[row][col];
+            }
+            else
+                map[indexes.second][indexes.first] = EMPTY_TILE;
+        }
+        map[indexes.second][0] = map[indexes.second][width - 2] = VERTICAL_BORDER_TILE;
     }
 
-    for (int c = 0; c < width - 1; ++c)
-        map[height - 1][c] = VERTICAL_BORDER_TILE;
+    for (indexes.first = 0; indexes.first < width - 1; ++indexes.first)
+        map[height - 1][indexes.first] = HORIZONTAL_BORDER_TILE;
 }
 
 void Window::clear() const
@@ -64,20 +132,20 @@ void Window::clear() const
 void Window::draw()
 {
     clear();
-    
-    LinkedList<Bullet>::iterator it = bullets.beg();
+    generate_map();
 
-    Pair<int, int> counters(0, 0);
-    for (counters.second; counters.second < map.get_height(); ++counters.second)
+    if (player.get_coords().first < width - (player.get_avatar().get_data().get_width() + 1))
+        player.move(1, 0);
+
+    int row = 0, col = 0;
+    Pair<int, int> indexes(0, 0);
+
+    for (indexes.second; indexes.second < height; ++indexes.second)
     {
-        counters.first = 0;
-        for (counters.first; counters.first < map.get_width(); ++counters.first)
+        for (indexes.first = 0; indexes.first < width; ++indexes.first)
         {
-            if (counters == (*it).get_coords())
-                (*it++).get_avatar().get_data().print();
-            else
-                std::cout << map[counters.second][counters.first];
+            printf("%c", map[indexes.second][indexes.first]);
         }
-        std::cout << "\n";
+        printf("\n");
     }
 }
--- Projects/SpacePewPews/window.h
+++ Projects/SpacePewPews/window.h
@@ -1,31 +1,49 @@
 #pragma once
+#include "utils/execeptions.h"
+
 #include "utils/containers/array2D.h"
 #include "utils/containers/linked_list.h"
 
+#include "entity.h"
 #include "bullet.h"
+#include "character.h"
 
 #define DEFAULT_HEIGHT 35
 #define DEFAULT_WIDTH 145
 
 #define EMPTY_TILE ' '
-#define HORIZONTAL_BORDER_TILE '|'
-#define VERTICAL_BORDER_TILE '-'
+#define VERTICAL_BORDER_TILE '|'
+#define HORIZONTAL_BORDER_TILE '-'
 
 class Window
 {
-  private:
-    int width, height;
-    array2D<char> map;
-    LinkedList<Bullet> bullets;
+private:
+  int width, height;
+  array2D<char> map;
 
-  public:
-    void generate_map();
-    void clear() const;
-    
-  public:
-    Window();
-    Window(const int, const int);
-    ~Window();
-  public:
-    void draw();
+  LinkedList<Bullet> bullets;
+  LinkedList<Character> enemies;
+  Character player;
+
+private:
+  void set_entity_starting_position(Entity *,
+                                    const int);
+
+public:
+  void generate_map();
+  void clear() const;
+
+#pragma region Big Four
+public:
+  Window();
+  Window(const Window &) = delete;
+  Window &operator=(const Window &) = delete;
+  ~Window();
+#pragma endregion
+
+public:
+  Window(const int, const int);
+
+public:
+  void draw();
 };
